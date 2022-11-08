51c4d19807df4a8bc52c4e16071d4f2c8c6a587e
gwallison <34752670+gwallison@users.noreply.github.com>
2022-11-08 08:08:02 -0500

    add disclosure list

--- summarize_data.py
+++ summarize_data.py
@@ -7,19 +7,27 @@ Created on Sun Nov  6 20:21:24 2022
 Used to make
 """
 import pandas as pd
+from IPython.display import display
+# from IPython.display import Markdown as md
 
 
 def summarize_api_set(lst,df):
     upk = df[df.api10.isin(lst)].UploadKey.unique().tolist()
     print(f'Number of disclosures within range: {len(upk)} ')
+    gb = df[df.UploadKey.isin(upk)].groupby('UploadKey',as_index=False)[['APINumber',
+                                                                         'WellName',
+                                                                         'date',
+                                                                         'OperatorName',
+                                                                         'TotalBaseWaterVolume',]].first()
+    display(gb)
     #print(df.columns)
     df['year'] = df.date.dt.year
     t = df[df.UploadKey.isin(upk)]
     mg = pd.merge(t.groupby('bgCAS',as_index=False)['calcMass'].sum(),
                   t.groupby('bgCAS',as_index=False)['epa_pref_name'].first(),
                   on='bgCAS',how='inner')
-    mg1 = pd.merge(t.groupby('bgCAS',as_index=False)['year'].min().rename({'year':'min'},axis=1),
-                  t.groupby('bgCAS',as_index=False)['year'].max().rename({'year':'max'},axis=1),
+    mg1 = pd.merge(t.groupby('bgCAS',as_index=False)['year'].min().rename({'year':'min_year'},axis=1),
+                  t.groupby('bgCAS',as_index=False)['year'].max().rename({'year':'max_year'},axis=1),
                   on='bgCAS',how='inner')
     
-    return pd.merge(mg,mg1,on='bgCAS',how='inner')
+    display(pd.merge(mg,mg1,on='bgCAS',how='inner'))
