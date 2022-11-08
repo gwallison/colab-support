dd66a110e723780f346cccd8256a65bf83930ef4
gwallison <34752670+gwallison@users.noreply.github.com>
2022-11-08 13:17:49 -0500

    test

--- summarize_data.py
+++ summarize_data.py
@@ -21,13 +21,13 @@ def summarize_api_set(lst,df):
                                                                          'TotalBaseWaterVolume',]].first()
     display(gb)
     #print(df.columns)
-    df['year'] = df.date.dt.year
-    t = df[df.UploadKey.isin(upk)]
-    mg = pd.merge(t.groupby('bgCAS',as_index=False)['calcMass'].sum(),
-                  t.groupby('bgCAS',as_index=False)['epa_pref_name'].first(),
-                  on='bgCAS',how='inner')
-    mg1 = pd.merge(t.groupby('bgCAS',as_index=False)['year'].min().rename({'year':'min_year'},axis=1),
-                  t.groupby('bgCAS',as_index=False)['year'].max().rename({'year':'max_year'},axis=1),
-                  on='bgCAS',how='inner')
+    # df['year'] = df.date.dt.year
+    # t = df[df.UploadKey.isin(upk)]
+    # mg = pd.merge(t.groupby('bgCAS',as_index=False)['calcMass'].sum(),
+    #               t.groupby('bgCAS',as_index=False)['epa_pref_name'].first(),
+    #               on='bgCAS',how='inner')
+    # mg1 = pd.merge(t.groupby('bgCAS',as_index=False)['year'].min().rename({'year':'min_year'},axis=1),
+    #               t.groupby('bgCAS',as_index=False)['year'].max().rename({'year':'max_year'},axis=1),
+    #               on='bgCAS',how='inner')
     
-    display(pd.merge(mg,mg1,on='bgCAS',how='inner'))
+    # display(pd.merge(mg,mg1,on='bgCAS',how='inner'))
