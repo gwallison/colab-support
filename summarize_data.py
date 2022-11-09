b02d1d1000877a21c0d8ea8dc1fff65f9919c642
gwallison <34752670+gwallison@users.noreply.github.com>
2022-11-09 06:42:30 -0500

    test

--- summarize_data.py
+++ summarize_data.py
@@ -9,7 +9,8 @@ Used to make
 import pandas as pd
 from IPython.display import display
 # from IPython.display import Markdown as md
-
+from google.colab import data_table
+data_table.enable_dataframe_formatter()
 
 def summarize_api_set(lst,df):
     upk = df[df.api10.isin(lst)].UploadKey.unique().tolist()
@@ -19,15 +20,16 @@ def summarize_api_set(lst,df):
                                                                          'date',
                                                                          'OperatorName',
                                                                          'TotalBaseWaterVolume',]].first()
-    display(gb)
-    #print(df.columns)
-    # df['year'] = df.date.dt.year
-    # t = df[df.UploadKey.isin(upk)]
-    # mg = pd.merge(t.groupby('bgCAS',as_index=False)['calcMass'].sum(),
-    #               t.groupby('bgCAS',as_index=False)['epa_pref_name'].first(),
-    #               on='bgCAS',how='inner')
-    # mg1 = pd.merge(t.groupby('bgCAS',as_index=False)['year'].min().rename({'year':'min_year'},axis=1),
-    #               t.groupby('bgCAS',as_index=False)['year'].max().rename({'year':'max_year'},axis=1),
-    #               on='bgCAS',how='inner')
+    data_table.DataTable(gb, include_index=False, num_rows_per_page=25)
+
+    # print(df.columns)
+    df['year'] = df.date.dt.year
+    t = df[df.UploadKey.isin(upk)]
+    mg = pd.merge(t.groupby('bgCAS',as_index=False)['calcMass'].sum(),
+                  t.groupby('bgCAS',as_index=False)['epa_pref_name'].first(),
+                  on='bgCAS',how='inner')
+    mg1 = pd.merge(t.groupby('bgCAS',as_index=False)['year'].min().rename({'year':'min_year'},axis=1),
+                  t.groupby('bgCAS',as_index=False)['year'].max().rename({'year':'max_year'},axis=1),
+                  on='bgCAS',how='inner')
     
-    # display(pd.merge(mg,mg1,on='bgCAS',how='inner'))
+    display(pd.merge(mg,mg1,on='bgCAS',how='inner'))
